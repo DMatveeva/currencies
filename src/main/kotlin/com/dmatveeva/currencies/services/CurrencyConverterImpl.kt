e336090b3fd1bd4fd0b7da1e2cbac0100fcd3058
DariaChernikova <darya.d.chernikova@gmail.com>
2022-01-11 22:41:47 +0300

    update

--- src/main/kotlin/com/dmatveeva/currencies/services/CurrencyConverterImpl.kt
+++ src/main/kotlin/com/dmatveeva/currencies/services/CurrencyConverterImpl.kt
@@ -10,12 +10,14 @@ import java.net.http.HttpResponse
 class CurrencyConverterImpl : CurrencyConverter {
 
     override fun convert(from: String, to: String): String {
-        val client = HttpClient.newBuilder().build();
+       /* val client = HttpClient.newBuilder().build();
         val request = HttpRequest.newBuilder()
+            .uri(URI.create("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/$from/$to.json"))
             .uri(URI.create("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/$from/$to.json"))
             .build();
 
         val response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        return response.body();*/
+        return  "Hello!"
     }
 }
